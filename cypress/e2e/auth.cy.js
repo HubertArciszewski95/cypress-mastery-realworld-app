32676b1de1082df1cf525cf4143af1102e516999
Hubert Arciszewski <hubertarciszewski@gmail.com>
2023-10-19 18:03:18 +0200

    Testing page navigation

--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -1,4 +1,5 @@
 import { faker } from "@faker-js/faker";
+import standardUser from "../fixtures/users/standard.json";
 
 describe('Authorization', () => {
 
@@ -11,6 +12,10 @@ describe('Authorization', () => {
 
     beforeEach(() => {
       cy.visit("/register");
+    });
+
+    it('should display correct link to login page', () => {
+
     });
 
     it('should redirect to home page after registering, as logged in', () => {
@@ -27,8 +32,42 @@ describe('Authorization', () => {
       cy.getByTestId("nav-item").should("contain", username);
     });
 
+    it('should display register form validation errors', () => {
+      const wrongEmailFormat = [
+        "plainaddress",      // Missing @ sign and domain
+        "@domain.com",       // Missing username
+        "email.domain.com",  // Missing @
+        "email@domain",      // Missing top level domain (.com/.net/.org/etc)
+      ];
+
+      cy.getByTestId("username-input").type("Jon Snow");
+      cy.getByTestId("username-input").clear().blur();
+
+      cy.getByTestId("username-validation-msg").should("be.visible").and("have.text", "Username is required");
+
+      cy.getByTestId("email-input").type("JonSnow@example.com");
+      cy.getByTestId("email-input").clear().blur();
+
+      cy.getByTestId("email-validation-msg").should("be.visible").and("have.text", "Email is required");
+
+      cy.getByTestId("password-input").type("s3cret");
+      cy.getByTestId("password-input").clear().blur();
+
+      cy.getByTestId("password-validation-msg").should("be.visible").and("have.text", "Password is required");
+
+      cy.getByTestId("password-input").type("abc");
+      cy.getByTestId("password-validation-msg").should("be.visible").and("have.text", "Password must have a minimum 4 characters");
+
+      wrongEmailFormat.forEach((email) => {
+        cy.getByTestId("email-input").clear();
+        cy.getByTestId("email-input").type(email);
+
+        cy.getByTestId("email-validation-msg").should("be.visible").and("have.text", "Incorrect email format");
+      });
+    });
+
     it('should display error for taken username', () => {
-      cy.getByTestId("username-input").type("Cypress user");
+      cy.getByTestId("username-input").type(standardUser.username);
       cy.getByTestId("email-input").type("notExist@example.com");
       cy.getByTestId("password-input").type("password");
       cy.getByTestId("signup-btn").click();
@@ -38,7 +77,7 @@ describe('Authorization', () => {
 
     it('should display error for taken email', () => {
       cy.getByTestId("username-input").type("notExist");
-      cy.getByTestId("email-input").type("cypressUser@example.com");
+      cy.getByTestId("email-input").type(standardUser.email);
       cy.getByTestId("password-input").type("password");
       cy.getByTestId("signup-btn").click();
 
@@ -48,5 +87,43 @@ describe('Authorization', () => {
 
   context('Login', () => {
 
+    beforeEach(() => {
+      cy.visit("/login");
+    });
+
+    it('should display correct link to register page', () => {
+
+    });
+
+    it('should be able to login and logout', () => {
+      cy.getByTestId("email-input").type(standardUser.email);
+      cy.getByTestId("password-input").type(standardUser.password);
+      cy.getByTestId("signin-btn").click();
+
+      cy.location("hash").should("equal", "#/");
+      cy.getByTestId("nav-item").should("contain", standardUser.username);
+
+      cy.getByTestId("nav-item").last().click();
+      cy.getByTestId("dropdown-item").contains("Logout").click();
+
+      cy.location("hash").should("equal", "#/");
+      cy.getByTestId("nav-item").should("not.contain", standardUser.username);
+    });
+
+    it('should display error for not existing user', () => {
+      cy.getByTestId("email-input").type("notExisting@email.com");
+      cy.getByTestId("password-input").type("invalidPassword");
+      cy.getByTestId("signin-btn").click();
+
+      cy.getByTestId("error-message").should("be.visible").and("have.text", "Email or password is invalid");
+    });
+
+    it('should display error for existing user when invalid password', () => {
+      cy.getByTestId("email-input").type(standardUser.email);
+      cy.getByTestId("password-input").type("invalidPassword");
+      cy.getByTestId("signin-btn").click();
+
+      cy.getByTestId("error-message").should("be.visible").and("have.text", "Email or password is invalid");
+    });
   });
 })
