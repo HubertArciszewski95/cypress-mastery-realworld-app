60e0438c83858cc7dc1196531c230a4123dfb3fa
Hubert Arciszewski <hubertarciszewski@gmail.com>
2023-10-21 11:21:13 +0200

    Solution start - Test header navigation

---
+++ cypress/e2e/header.cy.js
@@ -0,0 +1,37 @@
+describe('Header navigation', () => {
+
+	context('Not logged in', () => {
+		it('should display correct navigation options and links', () => {
+			// Check if only 3 navigation options are visible in the header.
+
+			// Then check if the:
+			// - "Home" option is visible
+			// - "Sign in" option is visible
+			// - "Sign up" option is visible
+
+			// Also check if navigation for those options works as expected.
+		});
+	});
+
+	context('Logged in', () => {
+		it('should display correct navigation options and links', () => {
+			// Check if only 3 navigation options are visible in the header.
+			// Check if only 3 dropdown options exist in dropdown menu.
+
+			// Then check if the:
+			// - "Home" option is visible
+			// - "New Article" option is visible
+			// - "Username" option is visible
+
+			// Also check if navigation for those options works as expected.
+			// For "Username" option check if there will be no redirecting.
+
+			// Then check if the dropdown menu contains expected options:
+			// - "Profile" option exist
+			// - "Settings" option exist
+			// - "Logout" option exist
+
+			// Also check if navigation for those options works as expected.
+		});
+	});
+});
