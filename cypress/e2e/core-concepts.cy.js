7dfaaac815ebd0785477b6516f6781a8416ecefb
Hubert Arciszewski <hubertarciszewski@gmail.com>
2023-11-09 18:14:42 +0100

    Lesson end: Subject management part 1

--- cypress/e2e/core-concepts.cy.js
+++ cypress/e2e/core-concepts.cy.js
@@ -3,9 +3,10 @@ describe('Cypress core concepts', () => {
     it('Subject management', () => {
         cy.visit("/");
 
-        const home = cy.getByTestId("nav-item").eq(0);
-        // do some more actions...
-        // ...
-        cy.getByTestId("nav-item").eq(0).should("have.text", home);
+        cy.getByTestId("nav-item").eq(0).invoke("text").then(($homeText) => {
+            // do some more actions...
+            // ...
+            cy.getByTestId("nav-item").eq(0).should("have.text", $homeText);
+        });
     });
 });
