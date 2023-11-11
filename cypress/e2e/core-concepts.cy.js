650cbeb9166bc7eeff83d177c36e324420aabb3d
Hubert Arciszewski <hubertarciszewski@gmail.com>
2023-11-11 09:39:49 +0100

    Lesson end: Subject management - Part 2

--- cypress/e2e/core-concepts.cy.js
+++ cypress/e2e/core-concepts.cy.js
@@ -5,9 +5,25 @@ describe('Cypress core concepts', () => {
         cy.visit("/");
 
         cy.contains("button", "Global Feed").click();
-		
-		// 1. Get value of likes
-        // 2. Click like button
-        // 3. Assert if like value changed
-     });
+
+        // cy.get(".article-preview").eq(0).find("button").then(($likeButton) => {
+        //     // 1. Get value of likes
+        //     const likesBeforeAction = parseInt($likeButton.text());
+
+        //     // 2. Click like button
+        //     cy.wrap($likeButton).click();
+
+        //     // 3. Assert if like value changed
+        //     cy.wrap($likeButton)
+        //         .invoke("text")
+        //         .then(parseInt)
+        //         .should("equal", likesBeforeAction + 1);
+
+        cy.get(".article-preview").eq(0).find("button").click();
+        cy.get(".article-preview").eq(0).find("button").invoke("text").then(($likes) => {
+            cy.wrap($likes)
+                .then(parseInt)
+                .should("equal", 1);
+        });
+    });
 });
