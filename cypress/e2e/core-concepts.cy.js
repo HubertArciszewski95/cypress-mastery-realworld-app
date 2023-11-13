12bb0baed80d4ce2176acf177c8952ca26897d41
Hubert Arciszewski <hubertarciszewski@gmail.com>
2023-11-13 17:57:41 +0100

    Solution start: Manage the subject

--- cypress/e2e/core-concepts.cy.js
+++ cypress/e2e/core-concepts.cy.js
@@ -1,29 +1,19 @@
 describe('Cypress core concepts', () => {
 
-    it('Subject management', () => {
-        cy.login("cypressUser@example.com", "s3cret");
-        cy.visit("/");
+    it('Task 1', () => {
+        // 1. Go to home page
+        // 2. Get date from first article for later use,
+        // 3. Go to first article details,
+        // Check if: 
+        // 4. Date in article details is the same as date on article preview.
+    });
 
-        cy.contains("button", "Global Feed").click();
-
-        // cy.get(".article-preview").eq(0).find("button").then(($likeButton) => {
-        //     // 1. Get value of likes
-        //     const likesBeforeAction = parseInt($likeButton.text());
-
-        //     // 2. Click like button
-        //     cy.wrap($likeButton).click();
-
-        //     // 3. Assert if like value changed
-        //     cy.wrap($likeButton)
-        //         .invoke("text")
-        //         .then(parseInt)
-        //         .should("equal", likesBeforeAction + 1);
-
-        cy.get(".article-preview").eq(0).find("button").click();
-        cy.get(".article-preview").eq(0).find("button").invoke("text").then(($likes) => {
-            cy.wrap($likes)
-                .then(parseInt)
-                .should("equal", 1);
-        });
+    it('Task 2', () => {
+        // 1. Go to home page
+        // 2. From second article get
+        //  "finances" and "technology" tags for later use,
+        // 3. Go to second article details
+        // Check if: 
+        // 4. Tags in article details match tags from article preview.
     });
 });
