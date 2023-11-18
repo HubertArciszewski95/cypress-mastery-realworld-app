3b6e97e63835cc403e802951325bf1a8bcdaaebe
Hubert Arciszewski <hubertarciszewski@gmail.com>
2023-11-18 14:51:50 +0100

    Solution end: Manage the subject

--- cypress/e2e/core-concepts.cy.js
+++ cypress/e2e/core-concepts.cy.js
@@ -1,19 +1,26 @@
 describe('Cypress core concepts', () => {
 
     it('Task 1', () => {
-        // 1. Go to home page
-        // 2. Get date from first article for later use,
-        // 3. Go to first article details,
-        // Check if: 
-        // 4. Date in article details is the same as date on article preview.
+        cy.visit("/");
+
+        cy.get(".article-preview").eq(0).find(".date").invoke("text").then(($articleDate) => {
+            cy.get(".article-preview").eq(0).contains("span", "Read more...").click();
+            
+            cy.get(".date").eq(0).should("have.text", $articleDate);
+        });
     });
 
     it('Task 2', () => {
-        // 1. Go to home page
-        // 2. From second article get
-        //  "finances" and "technology" tags for later use,
-        // 3. Go to second article details
-        // Check if: 
-        // 4. Tags in article details match tags from article preview.
+        cy.visit("/");
+        
+        cy.get(".article-preview").eq(1).find(".tag-pill").eq(0).invoke("text").then(($financesTag) => {
+            cy.get(".article-preview").eq(1).find(".tag-pill").eq(1).invoke("text").then(($technologyTag) => {
+                
+                cy.get(".article-preview").eq(1).contains("span", "Read more...").click();
+                
+                cy.get(".tag-pill").eq(0).should("have.text", $financesTag);
+                cy.get(".tag-pill").eq(1).should("have.text", $technologyTag);
+            });
+        });
     });
 });
