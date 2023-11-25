6cf538b1e652f46d612d85cb29a4f170d0d3f5f3
Hubert Arciszewski <hubertarciszewski@gmail.com>
2023-11-25 13:02:26 +0100

    Lesson start: Aliases (Query, Static)

--- cypress/e2e/core-concepts.cy.js
+++ cypress/e2e/core-concepts.cy.js
@@ -1,26 +1,7 @@
 describe('Cypress core concepts', () => {
 
-    it('Task 1', () => {
+    it('Alias (Query, Static)', () => {
         cy.visit("/");
 
-        cy.get(".article-preview").eq(0).find(".date").invoke("text").then(($articleDate) => {
-            cy.get(".article-preview").eq(0).contains("span", "Read more...").click();
-            
-            cy.get(".date").eq(0).should("have.text", $articleDate);
-        });
-    });
-
-    it('Task 2', () => {
-        cy.visit("/");
-        
-        cy.get(".article-preview").eq(1).find(".tag-pill").eq(0).invoke("text").then(($financesTag) => {
-            cy.get(".article-preview").eq(1).find(".tag-pill").eq(1).invoke("text").then(($technologyTag) => {
-                
-                cy.get(".article-preview").eq(1).contains("span", "Read more...").click();
-                
-                cy.get(".tag-pill").eq(0).should("have.text", $financesTag);
-                cy.get(".tag-pill").eq(1).should("have.text", $technologyTag);
-            });
-        });
     });
 });
