211a91c53ecbb2fefa5a69276b541cdf7f2e8ee1
Hubert Arciszewski <hubertarciszewski@gmail.com>
2023-12-28 13:37:39 +0100

    Lesson start: Iterate over DOM elements - Examples

--- cypress/e2e/core-concepts.cy.js
+++ cypress/e2e/core-concepts.cy.js
@@ -1,12 +1,11 @@
 describe('Cypress core concepts', () => {
 
-    it('Iterate over DOM elements', () => {
+    it('Iterate over DOM elements - Examples', () => {
         cy.visit("/");
 
         cy.get(".article-preview").should("have.length", 3).each(($article, index) => {
             const title = $article.find("h1").text();
 
-            cy.log(`Title: "${title}" of article ${index}`);
         });
     });
 });
