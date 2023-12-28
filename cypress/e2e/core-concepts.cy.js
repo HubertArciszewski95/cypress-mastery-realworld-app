7a382c2d699757f41dc0babbe02f42ae2eedb5ac
Hubert Arciszewski <hubertarciszewski@gmail.com>
2023-12-28 13:32:21 +0100

    Lesson end: Iterate over DOM elements

--- cypress/e2e/core-concepts.cy.js
+++ cypress/e2e/core-concepts.cy.js
@@ -3,8 +3,10 @@ describe('Cypress core concepts', () => {
     it('Iterate over DOM elements', () => {
         cy.visit("/");
 
-        cy.get(".article-preview").should("have.length", 3).forEach(($article) => {
+        cy.get(".article-preview").should("have.length", 3).each(($article, index) => {
+            const title = $article.find("h1").text();
 
+            cy.log(`Title: "${title}" of article ${index}`);
         });
     });
 });
