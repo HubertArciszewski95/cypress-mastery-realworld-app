c11e470d3dedb2abe4882c7400de3c02736c7768
Hubert Arciszewski <hubertarciszewski@gmail.com>
2023-12-30 14:02:25 +0100

    Lesson start: Test isolation

--- cypress/e2e/core-concepts.cy.js
+++ cypress/e2e/core-concepts.cy.js
@@ -1,15 +1,11 @@
 describe('Cypress core concepts', () => {
 
-    it('Iterate over DOM elements - Examples', () => {
-        cy.visit("https://www.saucedemo.com/v1/inventory.html");
+    it('Test 1', () => {
+        cy.login("cypressUser@example.com", "s3cret");
+        cy.visit("/");
+    });
 
-        let totalPrice = 0;
-        cy.get(".inventory_item_price").should("be.visible").each(($price) => {
-            const price = parseFloat($price.text().replace("$", ""));
-            
-            totalPrice += price;
-        }).then(() => {
-            cy.log(`The total price is: $${totalPrice}`);
-        });
+    it('Test 2', () => {
+        cy.getByTestId("nav-item").should("contain", "Cypress user");
     });
 });
