0f62604ebffa052bc39bc75f27e945e4c5633a47
Hubert Arciszewski <hubertarciszewski@gmail.com>
2024-01-06 17:27:48 +0100

    Lesson end: Implementing Page Object - Part 1

---
+++ cypress/page-object/login-page.js
@@ -0,0 +1,23 @@
+class LoginPage {
+
+    elements = {
+        emailInput: () => cy.getByTestId("email-input"),
+        passwordInput: () => cy.getByTestId("password-input"),
+        signInButton: () => cy.getByTestId("signin-btn"),
+        errorMessage: () => cy.getByTestId("error-message"),
+    }
+
+    typeEmail(email) {
+        this.elements.emailInput().type(email);
+    }
+
+    typePassword(password) {
+        this.elements.passwordInput().type(password);
+    }
+
+    clickSignInButton() {
+        this.elements.signInButton().click();
+    }
+}
+
+export default LoginPage;
