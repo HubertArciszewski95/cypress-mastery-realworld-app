01f658ede94c63331ed1692fd20087ea94973b59
Hubert Arciszewski <hubertarciszewski@gmail.com>
2024-02-24 11:39:23 +0100

    Lesson end: NPM scripts

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  viewportHeight: 720,
+  viewportWidth: 1280,
   e2e: {  
     baseUrl: "http://localhost:3000/#",
     watchForFileChanges: false,
-    viewportHeight: 720,
-    viewportWidth: 1280,
   
     setupNodeEvents(on, config) {
       // implement node event listeners here
