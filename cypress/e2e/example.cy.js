33b0bed0efeb306b8ea080a10c7abf81bfe99344
Hubert Arciszewski <hubertarciszewski@gmail.com>
2024-03-30 12:27:02 +0100

    Lesson start: Private methods

--- cypress/e2e/example.cy.js
+++ cypress/e2e/example.cy.js
@@ -4,8 +4,6 @@ describe('Example', () => {
     const loginPage = new LoginPage();
 
     it('test', () => {
-        cy.visit("/");
-
-        loginPage.elements.emailInput({ timeout: 10_000 }).should("be.visible");
+        
     });
 });
---
+++ cypress/page-object/trade-report-page.js
@@ -0,0 +1,39 @@
+class TradeReportPage {
+
+    elements = {
+        // Some elements...
+    }
+
+    typeReportName(reportName) {
+        cy.log("Type report name");
+        // Some logic...
+    }
+
+    selectAssetClass(assetClass) {
+        cy.log("Select asset class");
+        // Some logic...
+    }
+
+    selectCurrency(currency) {
+        cy.log("Select currency");
+        // Some logic...
+    }
+
+    selectTradeType(tradeType) {
+        cy.log("Select trade type");
+        // Some logic...
+    }
+
+    selectCountry(countryName) {
+        cy.log("Select country");
+        // Some logic...
+    }
+
+    clickGenerateReport() {
+        cy.log("Click generate report");
+        // Some logic...
+    }
+
+}
+
+export default TradeReportPage;
